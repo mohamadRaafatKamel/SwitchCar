65bd43c2f0db0f4538aa4587ede2465a79a535a2
mohamad <42893895+mohamadRaafatKamel@users.noreply.github.com>
2021-01-05 06:01:43 +0200

    Design Reset

--- views/site/_car_item.php
+++ views/site/_car_item.php
@@ -13,13 +13,21 @@ use app\components\MyStrings;
 ?>
 
 <!--start-->
-<div class="movie-item-style-2">
+<div class="movie-item-style-2 shadow ">
     <img src="<?= $cover  ?>" alt="">
     <div class="mv-item-infor">
-        <h6><a href="<?= Url::to(['viewcar', 'id' => $model->cid]); ?>"> <?= $model->cname ?></a></h6>
+        <h6>
+            <a href="<?= Url::to(['viewcar', 'id' => $model->cid]); ?>">
+                <span><?= $model->cbrand ?></span>
+                <span><?= $model->cname ?></span>
+<!--                <span>--><?php // $model->ct->ctname ?><!--</span>-->
+                <span><?= $model->machen ?></span>
+                <span><?= $model->cmodel ?></span>
+            </a>
+        </h6>
         <p class="describe"><?= $model->descrp ?></p>
         <p class="run-time">
-            <span>اللون : احمر </span>     .
+            <span>اللون : gg </span>     .
             <span>العجل : 4 </span>    .
             <span>الموديل : 5252</span>
         </p>
